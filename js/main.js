b5c4a359026cd8fd7c2ca043f24b9cc8720f83c8
universaldeclaration <universaldeclaration@outlook.com>
2021-08-11 22:08:26 +0800

    update display mode

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
 // mian.js all style change
 
+$(document).ready(function() {
+
+    // 添加切换模式按钮
+    $(".navbar-brand").after("" +
+        "<div class='text-right'>" +
+        "<a class='btn btn-sm'>Light Mode</a>" +
+        "<span>&nbsp;&nbsp;</span>" +
+        "<a class='btn btn-sm'>Dark Mode</a>" +
+        "</div>");
+
+    // 切换页面显示模式
+    $("a.btn").click(function() {
+        if($(this).text() === "Light Mode") {
+            $("body").removeClass("bg-dark");
+            $("div, a").removeClass("link-light").removeClass("link-light").removeClass("bg-dark");
+            $("a").addClass("link-dark").attr("displaymode", "").attr("displaymode", "light");
+            $("a.navbar-brand").removeClass("text-light");
+        }
+        if($(this).text() === "Dark Mode") {
+            $("body").addClass("bg-dark");
+            $("div, div.small, a").removeClass("link-dark").addClass("link-light").addClass("bg-dark");
+            $("a").attr("displaymode", "").attr("displaymode", "dark");
+            $("a.navbar-brand").addClass("text-light");
+        }
+    });
+});
